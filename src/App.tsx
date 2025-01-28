cf3c86a6a70a03d1488b083d60fd48e692104c6c
Yago Barbosa <91913602+YagoB16@users.noreply.github.com>
2025-01-27 21:20:03 -0300

    Adição de variáveis globais; Inclusão de styled-components;

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from 'styled-components';
 import { Button } from "./components/Button";
 import { defaultTheme } from './styles/themes/default';
+import { GlobalStyle } from './styles/global';
 export function App() {
     return (
         <ThemeProvider theme={defaultTheme}>
@@ -9,6 +10,8 @@ export function App() {
             <Button variant="success" />
             <Button variant="danger" />
             <Button />
+
+            <GlobalStyle/>
         </ThemeProvider>
     )
 }
